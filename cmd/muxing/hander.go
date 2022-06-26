6471c26272038d3fe65b196635172be96952e85b
viktor <viktor@localhost>
2022-06-26 14:35:02 +0400

    solution

---
+++ cmd/muxing/hander.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+func createRouter() *mux.Router {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	router.HandleFunc("/name/{name}", func(w http.ResponseWriter, r *http.Request) {
+		args := mux.Vars(r)
+
+		if _, ok := args["name"]; !ok {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf("Hello, %s!", args["name"])))
+	}).Methods(http.MethodGet)
+
+	router.HandleFunc("/bad", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusInternalServerError)
+	}).Methods(http.MethodGet)
+
+	router.HandleFunc("/data", func(w http.ResponseWriter, r *http.Request) {
+		data, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Write(append([]byte("I got message:\n"), data...))
+	}).Methods(http.MethodPost)
+
+	router.HandleFunc("/headers", func(w http.ResponseWriter, r *http.Request) {
+		headers := r.Header
+		if headers == nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		if _, ok := headers["a"]; !ok {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		if _, ok := headers["b"]; !ok {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		headerA := headers["a"][0]
+		headerB := headers["b"][0]
+
+		intA, err := strconv.Atoi(headerA)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		intB, err := strconv.Atoi(headerB)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		w.Header().Add(
+			fmt.Sprintf("\"%s+%s\"", headerA, headerB),
+			fmt.Sprintf("\"%d\"", intA+intB),
+		)
+
+		w.WriteHeader(http.StatusOK)
+	}).Methods(http.MethodPost)
+
+	return router
+}
--- cmd/muxing/muxing.go
+++ cmd/muxing/muxing.go
@@ -6,8 +6,6 @@ import (
 	"net/http"
 	"os"
 	"strconv"
-
-	"github.com/gorilla/mux"
 )
 
 /**
@@ -19,7 +17,7 @@ main function reads host/port from env just for an example, flavor it following 
 
 // Start /** Starts the web server listener on given host and port.
 func Start(host string, port int) {
-	router := mux.NewRouter()
+	router := createRouter()
 
 	log.Println(fmt.Printf("Starting API server on %s:%d\n", host, port))
 	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), router); err != nil {
